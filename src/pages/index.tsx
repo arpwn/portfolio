acd1bc14c0e0f166f8278f2daf078ca79e8a797d
nery <wespenry@hotmail.com>
2024-01-12 17:49:53 -0600

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,17 @@ export default function Home() {
           <title>Armando&apos;s Portfolio</title>
           <meta name="description" content="Personal portfolio to showcase my projects." />
           <link rel="icon" href="/favicon.ico" />
+          {/* Hotjar */}
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3824120,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
         </Head>
         <div className="main-content">
           <div id="home">
